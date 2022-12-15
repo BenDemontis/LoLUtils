422acf9593334c34b04465885dba0d29c5d7e99e
BenDemontis <benjamin.demontis1@gmail.com>
2022-12-15 14:08:52 +0000

    Basic bot proof of concept start.

---
+++ bot.py
@@ -0,0 +1,53 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+from discord.ext import tasks
+
+
+import dotenv
+import os
+import time
+
+#Loads file containing the environment variables with secrets
+dotenv.load_dotenv()
+
+#Sets the intents that the bot will subscribe to, to get events from
+intents = discord.Intents.default()
+#Sets seeing the message content to true
+intents.message_content = True
+
+#Creates a discord client instance
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.command()
+async def test(ctx, *args):
+    arguments = ','.join(args)
+    await ctx.send(f'{len(args)} arguments: {arguments}')
+
+#Lookup guide for slash commands using discord.ext.commands
+#@bot.command(name='help2',description='provides help to the user')
+#async def help2(ctx, arg: str):
+#    await ctx.send(f'Preparing the help function for the {arg} provided....')
+
+
+#Code to run upon bot being ready and logged in and setup
+@bot.event
+async def on_ready():
+    await bot.load_extension('unlq')
+    await bot.tree.sync(guild=None)
+    print(f'We have logged in as {bot.user}')
+
+
+#Code to run upon a message being recieved that the client can see.
+#@bot.event
+#async def on_message(message):
+#    if message.author == bot.user:
+#        return
+    
+#    if message.content.startswith('$hello'):
+#        await message.channel.send('Hello!')
+
+
+
+#Runs the client using the discord bot token saved in the env variable.
+bot.run(os.getenv('DISCORD_TOKEN'))
---
+++ unlq.py
@@ -0,0 +1,25 @@
+from discord.ext import commands
+from discord.ext import tasks
+import discord
+
+#class helpFlags(commands.FlagConverter):
+#    command: str = commands.flag(description='Command that info is desired for')
+
+
+class unlqCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self._last_member = None
+        bot.remove_command("help")
+
+    @commands.hybrid_command()
+    #async def help(ctx, *, flags: helpFlags):
+    async def help(self, ctx, command):
+        if(command=='test'):
+            await ctx.send(f'The command you wanted help with was {command}.\nThis command is to test hybrid command functionality.')
+        else:
+            await ctx.send(f'The command you wanted help with was {command}.\nThis command is not currently recognised by the bot or has no help description.')
+
+
+async def setup(bot):
+    await bot.add_cog(unlqCog(bot))
